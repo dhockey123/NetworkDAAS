a7f804fa15e1eb2f85e7fed4f3e35dc3766de4bb
dhockey123 <dhockey123@github.com>
2023-05-27 05:53:01 +0100

    Implemented use of objective functions for network design solutions. New buttons. Implemented different problem types.

--- generate_lpsolve.py
+++ generate_lpsolve.py
@@ -1,34 +1,123 @@
-# path_flow_vars = [[[1], [3, 2], [3, 5, 4]], [[2],[1, 3], [4, 5]]]
-# link_capacities = [1,2,3,4,5]
-def gen_lp_solve_code(path_flow_vars, path_flow_DV, link_capacities):
-    print(path_flow_vars)
-    print(path_flow_DV)
-    f = open("lp_solve.txt", "w+")
-    f.write("// DEMAND CONSTRAINTS\n")
-    for i, flows in enumerate(path_flow_vars):
+import os
+
+def get_demand_constraints(path_flow_vars, path_flow_DV):
+    all_demand_constraints = ""
+    for i, demand in enumerate(path_flow_vars):
         demand_constraint = ""
-        ## DEMAND CONSTRAINTS
-        for x in range(len(flows)):
-            demand_constraint += "x%d%d+" % (i+1, x+1)
-
+        for x in range(len(demand)):
+            demand_constraint += "X%d%d+" % (i+1, x+1)
         demand_constraint = demand_constraint[:-1] + "=" +str(path_flow_DV[i]) + ";\n"
+        all_demand_constraints+=demand_constraint
+    return all_demand_constraints
 
-        f.write(demand_constraint)
-
-
-    f.write("\n// CAPACITY CONSTRAINTS\n")
-    ## CAPACITY CONSTRAINTS
+def get_capacity_constraints(path_flow_vars, link_capacities):
+    all_capacity_constraints = ""
     for x in range(1,len(link_capacities)+1):
         link_inequality = ""
         # print(x, end=" ")
         for i, demand_path in enumerate(path_flow_vars):
             for j, flow in enumerate(demand_path):
-                # print(j, flow, len(demand_path))
-            
                 if x in flow:
-                    link_inequality += "x%d%d+" % (i+1,j+1)
+                    link_inequality += "X%d%d+" % (i+1,j+1)
+        if link_capacities[x-1] != 0:
+            link_inequality = link_inequality[:-1] + "<=" + str(link_capacities[x-1]) + ";\n"
+        else:
+            link_inequality = link_inequality[:-1] + "<=y" + str(x) + ";\n"
+        all_capacity_constraints+=link_inequality
+    return all_capacity_constraints
 
-        link_inequality = link_inequality[:-1] + "<=" + str(link_capacities[x-1]) + ";\n"
-        f.write(link_inequality)
+def get_enforced_path_constraints(path_flow_vars, path_flow_DV, min_paths):
+    all_enf_path_constraints = ""
+    for i, demand in enumerate(path_flow_vars):
+        for j, flow in enumerate(demand):
+            all_enf_path_constraints += ("X%d%d<=%.2f;\n" %(i+1, j+1, path_flow_DV[i]/min_paths))
+    return all_enf_path_constraints
 
-# gen_lp_solve_code(path_flow_vars, path_flow_DV)
+def get_flows_in_use(path_flow_vars, path_flow_DV):
+    all_flows_in_use = ""
+    for i, demand in enumerate(path_flow_vars):
+        for j, flow in enumerate(demand):
+            all_flows_in_use += "X%d%d<=%dU%d%d;\n" % (i+1, j+1, path_flow_DV[i], i+1, j+1)
+    return all_flows_in_use
+
+def get_lower_bound_flows(path_flow_vars, min_flow_vol):
+    all_lower_bound_flows = ""
+    for i, demand in enumerate(path_flow_vars):
+        for j, flow in enumerate(demand):
+            all_lower_bound_flows += "%dU%d%d<=X%d%d;\n" % (min_flow_vol,  i+1, j+1, i+1, j+1)
+    return all_lower_bound_flows
+
+def solve_gen_dimen_alloc_prob(path_flow_vars, path_flow_DV, link_capacities, obj_func):
+    f = open("lp_solve.txt", "w+")
+    ## OBJECTIVE FUNCTION
+    f.write("// OBJECTIVE FUNCTION\n")
+    f.write(obj_func)
+
+    ## DEMAND CONSTRAINTS
+    f.write("\n// DEMAND CONSTRAINTS\n")
+    f.write(get_demand_constraints(path_flow_vars, path_flow_DV))
+
+    ## CAPACITY CONSTRAINTS
+    f.write("\n// CAPACITY CONSTRAINTS\n")
+    f.write(get_capacity_constraints(path_flow_vars, link_capacities))
+
+    f.close()
+    os.system("lp_solve "+os.getcwd()+"/lp_solve.txt")
+
+def solve_enforced_path_diversity(path_flow_vars, path_flow_DV, link_capacities, obj_func, min_paths):
+    f = open("lp_solve.txt", "w+")
+    ## OBJECTIVE FUNCTION
+    f.write("// OBJECTIVE FUNCTION\n")
+    f.write(obj_func)
+
+    ## DEMAND CONSTRAINTS
+    f.write("\n// DEMAND CONSTRAINTS\n")
+    f.write(get_demand_constraints(path_flow_vars, path_flow_DV))
+
+    ## CAPACITY CONSTRAINTS
+    f.write("\n// CAPACITY CONSTRAINTS\n")
+    f.write(get_capacity_constraints(path_flow_vars, link_capacities))
+
+    ## ENFORCED PATH DIVERSITY
+    f.write("\n// ENFORCED PATH DIVERSITY\n")
+    f.write(get_enforced_path_constraints(path_flow_vars, path_flow_DV, min_paths))
+
+    f.close()
+    os.system("lp_solve "+os.getcwd()+"/lp_solve.txt")
+
+def solve_min_flow_vol(path_flow_vars, path_flow_DV, link_capacities, min_flow_vol, obj_func):
+    f = open("lp_solve.txt", "w+")
+    ## OBJECTIVE FUNCTION
+    f.write("// OBJECTIVE FUNCTION\n")
+    f.write(obj_func)
+    
+    ## DEMAND CONSTRAINTS
+    f.write("\n// DEMAND CONSTRAINTS\n")
+    f.write(get_demand_constraints(path_flow_vars, path_flow_DV))
+
+    ## CAPACITY CONSTRAINTS
+    f.write("\n// CAPACITY CONSTRAINTS\n")
+    f.write(get_capacity_constraints(path_flow_vars, link_capacities))
+
+    ## FLOWS IN USE
+    f.write("\n// FLOWS IN USE\n")
+    f.write(get_flows_in_use(path_flow_vars, path_flow_DV))
+
+    ## MIN FLOW VOLUME
+    f.write("\n// MIN FLOW VOLUME\n")
+    f.write(get_lower_bound_flows(path_flow_vars, min_flow_vol))
+
+
+    f.close()
+    os.system("lp_solve "+os.getcwd()+"/lp_solve.txt")
+
+
+
+
+a = [[[1], [3, 2]], [[2], [1, 3]]]
+b = [5.0, 3.0]
+c = [3.0, 0, 7.0]
+d = "min: 2X11+10X12+4X21+8X22;\n"
+
+# solve_enforced_path_diversity(a, b, c, d, 2)
+solve_min_flow_vol(a, b, c, 3, d)
--- ui_assign_params.py
+++ ui_assign_params.py
@@ -1,12 +1,17 @@
 from tkinter import *
 from path_tool import path_finder, node_paths_to_demand_paths
-from generate_lpsolve import gen_lp_solve_code
+from generate_lpsolve import *
+from objective_functions import *
 link_cost_entries = []
 link_capacity_entries = []
 demand_entries = []
+min_flow_volume = 0
+min_num_paths_p_demand = 0
 row = 1
+obj_func_type = ""
 
 ## Globals are a mess, fix later
+## Passing around too many different arrays. USE dictionaries!
 
 def create_demand():
     global win, demand_entries
@@ -29,11 +34,14 @@ def remove_demand():
 
    
 def print_demand(links, win):
-    global link_cost_entries, link_capacity_entries, demand_entries
+    global link_cost_entries, link_capacity_entries, demand_entries, obj_func_type
+    global min_num_paths_p_demand, min_flow_volume
     path_flow_vars = []
     path_flow_DV = []
     link_capacities = []
-    print
+    link_costs = []
+    print(min_flow_volume.get())
+    print(min_num_paths_p_demand.get())
     idx = 0
     for i in range(0, len(demand_entries), 3):
         idx+=1
@@ -41,32 +49,55 @@ def print_demand(links, win):
         node_b = int(demand_entries[i+1].get())
         node_paths = path_finder(links, node_a, node_b)
         demand_paths = node_paths_to_demand_paths(links, node_paths)
-        # print("d{0} paths -> {1}".format(idx, demand_paths))
         
         path_flow_vars.append(demand_paths)
         path_flow_DV.append(float(demand_entries[i+2].get()))
     for i in range(len(link_capacity_entries)):
-        link_capacities.append(float(link_capacity_entries[i].get()))
-    # print(link_capacities)
+        try:
+            link_capacities.append(float(link_capacity_entries[i].get()))
+        except:
+            link_capacities.append(0)
+    for i in range(len(link_capacity_entries)):
+        try:
+            link_costs.append(float(link_cost_entries[i].get()))
+        except:
+            link_costs.append(0)
 
-    gen_lp_solve_code(path_flow_vars, path_flow_DV, link_capacities)
-    win.destroy()
-    link_cost_entries = []
-    link_capacity_entries = []
-    demand_entries = []
+    obj_func = min_hops(path_flow_vars)
+    # print("here->", obj_func)
 
+    match obj_func_type:
+        case "min_hops":
+            obj_func = min_hops(path_flow_vars)
+        case "min_routing_cost":
+            obj_func = min_routing_cost(path_flow_vars, link_costs)
+
+    solve_gen_dimen_alloc_prob(path_flow_vars, path_flow_DV, link_capacities, obj_func)
+    # win.destroy()
+    # link_cost_entries = []
+    # link_capacity_entries = []
+    # demand_entries = []
+
+def set_obj_func(var):
+    global obj_func_type
+    # var = var.get()
+    match int(var.get()):
+        case 1:
+            obj_func_type = "min_hops"
+        case 2:
+            obj_func_type = "min_routing_cost"
 
 def assign_params(links):
-    global row, win, demand_entries
-    link_cost_entries = []
+    global row, win, demand_entries, obj_func_type, min_flow_volume, min_num_paths_p_demand
     demand_entries = []
 
     win = Tk()
+    var = IntVar()
 
     win.geometry("300x670")
     win.title("")
     
-    
+    # LINK COSTS AND CAPACITIES
     Label(win, text="Link costs", font=('Helvetica', 12, 'bold')).grid(row=row, column=0, columnspan=6)
     Label(win, text="Link Capacities", font=('Helvetica', 12, 'bold')).grid(row=row, column=6, columnspan=6)
     for i in range(len(links)):
@@ -75,9 +106,18 @@ def assign_params(links):
         Label(win, text="e%d" % (i+1)).grid(row=row, column=0, columnspan=1)
         link_cost_entries[i].grid(row=row, column=1, columnspan=5)
  
-        link_capacity_entries.append(Entry(win, width= 8))
+        link_capacity_entries.append(Entry(win, width= 8, ))
         Label(win, text="c%d" % (i+1)).grid(row=row, column=6, columnspan=1)
         link_capacity_entries[i].grid(row=row, column=7, columnspan=4)
+    row+=1
+    # EXTRA PATH CONSTRAINTS
+    Label(win, text="Min flow volume").grid(row=row, column=0)
+    min_flow_volume = Entry(win, width=8)
+    min_flow_volume.grid(row=row, column = 3)
+    row+=1
+    Label(win, text="Min # paths per demand").grid(row=row, column=0)
+    min_num_paths_p_demand = Entry(win, width=8)
+    min_num_paths_p_demand.grid(row=row, column = 3)
     row+=1
     # DEMANDS
     Label(win, text="Demands", font=('Helvetica', 12, 'bold')).grid(row=row, column=0, columnspan=12)
@@ -86,11 +126,21 @@ def assign_params(links):
     Label(win, text="Node B").grid(row=row, column=4, columnspan=4)
     Label(win, text="DVUs").grid(row=row, column=8, columnspan=4)
     create_demand()
-
-
-    
-    Button(win, text="Create demand",font=('Helvetica', 12, 'bold'), command=create_demand).grid(row=100, column=0, columnspan=3)
-    Button(win, text="Remove demand",font=('Helvetica', 12, 'bold'), command=remove_demand).grid(row=100, column=6, columnspan=3)
-    Button(win, text="LP_SOLVE", font=('Helvetica', 12, 'bold'), command=lambda: print_demand(links, win)).grid(row=102, column=0, columnspan=12)
-
+    # RADIO BUTTONS
+    row+=1
+    n_row=100
+    r = Radiobutton(win, text="Min Hops", variable=var, value=1, command=lambda: set_obj_func(var))
+    r.grid(row=n_row, column=2, columnspan=4)
+    n_row+=1
+    r = Radiobutton(win, text="Min Routing Cost", variable=var, value=2, command=lambda: set_obj_func(var))
+    r.grid(row=n_row, column=2, columnspan=4)
+    ## BUTTONS
+    n_row+=1
+    Button(win, text="Create demand",font=('Helvetica', 12, 'bold'), command=create_demand).grid(row=n_row, column=0, columnspan=3)
+    Button(win, text="Remove demand",font=('Helvetica', 12, 'bold'), command=remove_demand).grid(row=n_row, column=6, columnspan=3)
+    n_row+=1
+    Button(win, text="LP_SOLVE", font=('Helvetica', 12, 'bold'), command=lambda: print_demand(links, win)).grid(row=n_row, column=0, columnspan=12)
+    n_row+=1
     win.mainloop()
+
+# assign_params([(1,2),(2,3),(1,3)])
